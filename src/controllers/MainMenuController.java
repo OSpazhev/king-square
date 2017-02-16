eea51a2e0d723f70f5ca12c281fb7d504e12a8a6
Oleksandr Spazhev <o.spazhev@gmail.com>
2017-02-16 16:39:40 +0200

    Added dialog before single player game, where asking player`s name.

--- src/controllers/MainMenuController.java
+++ src/controllers/MainMenuController.java
@@ -1,9 +1,41 @@
 package controllers;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class MainMenuController {
-    public void showDialogBeforeSinglePlayerGame(ActionEvent actionEvent)
-    {
+
+    private final int MIN_HEIGHT = 100;
+    private final int MIN_WIDTH  = 400;
+
+    public void showDialogBeforeSinglePlayerGame(ActionEvent actionEvent) {
+
+        try {
+
+            Stage stage = new Stage();
+
+            Parent root = FXMLLoader.load(getClass().getResource("../fxml/DialogBeforeStartSinglePlayerGame.fxml"));
+
+            stage.setTitle("Ім'я гравця");
+            stage.setMinWidth(MIN_WIDTH);
+            stage.setMinHeight(MIN_HEIGHT);
+            stage.setResizable(false);
+            stage.setScene(new Scene(root));
+            stage.initModality(Modality.WINDOW_MODAL);
+            stage.initOwner((((Node)actionEvent.getSource()).getScene().getWindow()));
+            stage.show();
+
+        } catch (IOException e) {
+
+            e.printStackTrace();
+
+        }
     }
 }
