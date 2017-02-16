956f4166bed9acbba9b3796b37f1104743ebd2d2
Oleksandr Spazhev <O.Spazhev@gmail.com>
2017-02-16 13:23:03 +0200

    Initial commit

---
+++ src/menu/MainMenu.java
@@ -0,0 +1,28 @@
+package menu;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class MainMenu extends Application {
+
+    private final int MIN_WIDTH = 300;
+    private final int MIN_HEIGHT = 275;
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("../fxml/MainMenu.fxml"));
+        primaryStage.setTitle("Hello World");
+        primaryStage.setMinWidth(MIN_WIDTH);
+        primaryStage.setMinHeight(MIN_HEIGHT);
+        primaryStage.setScene(new Scene(root, MIN_WIDTH, MIN_HEIGHT));
+        primaryStage.show();
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
